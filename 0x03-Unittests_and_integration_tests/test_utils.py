063b03b35687492e32446b8c3b75ad1e2035657c
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-13 19:51:14 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Test Utils Module"""
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +28,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """test exception"""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """test class for get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+            ])
+    @patch('utils.requests.get')
+    def test_get_json(self, url: str, ex_payload: Dict, mock_get) -> None:
+        """test method for get_json"""
+        mock_response = Mock()
+        mock_response.json.return_value = ex_payload
+        mock_get.return_value = mock_response
+        return_val = get_json(url)
+        mock_get.assert_called_once_with(url)
+        self.assertEqual(return_val, ex_payload)
