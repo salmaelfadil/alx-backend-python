b37355d698cad35097e6c4b892ee128158d2aa21
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-14 18:53:42 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test Client Module"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from typing import Dict
 from client import GithubOrgClient
@@ -22,3 +22,14 @@ class TestGithubOrgClient(unittest.TestCase):
         github_client = GithubOrgClient(val)
         github_client.org()
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{val}")
+
+    def test_public_repos_url(self) -> None:
+        """test for mocking property"""
+        with patch('client.GithubOrgClient.org',
+                new_callable=PropertyMock) as prop_mock:
+            prop_mock.return_value = {
+                    'repos_url': "https://api.github.com/users/example/repos",
+                    }
+            test = GithubOrgClient("example")
+            val = test._public_repos_url
+            self.assertEqual(prop_mock.return_value['repos_url'], val)
