a39d413517f7cd017b41426ad141b256e12d7b4d
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-14 18:24:37 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -10,17 +10,15 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """class fort testing client function"""
     @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"})])
+        ("google"),
+        ("abc")
+        ])
     @patch('client.get_json')
     def test_org(
             self,
             val: str,
-            expected: Dict[str, str],
-            mock_get_json) -> None:
+            mock_get_json):
         """test for org method"""
-        mock_get_json.return_value = expected
         github_client = GithubOrgClient(val)
-        return_val = github_client.org()
-        mock_org.assert_called_once_with(f"https://api.github.com/orgs/{val}")
-        self.assertEqual(return_val, expected)
+        github_client.org()
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{val}")
