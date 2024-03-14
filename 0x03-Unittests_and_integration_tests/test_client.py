597a944bc8cf6fb408782ff233c0fdec75af28ae
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-14 21:31:05 +0000

    .

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,21 +38,21 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(prop_mock.return_value['repos_url'], val)
 
     @patch('client.get_json')
-    def test_public_repos(
-            self,
-            mock_get_json) -> None:
-        """test for public repos"""
-        mock_payload = [
-                {"name": "string1"},
-                {"name": "string2"}]
+    def test_public_repos(self, mock_get_json):
+        """test method for public_repos"""
+        mock_payload = [{"name": "string1"}, {"name": "string2"}]
         mock_get_json.return_value = mock_payload
+
         with patch('client.GithubOrgClient._public_repos_url',
                    new_callable=PropertyMock) as prop_mock:
+
             prop_mock.return_value = "example"
-            test = GithubOrgClient("test")
+            test = GithubOrgClient('test')
             val = test.public_repos()
+
             lis = [item["name"] for item in mock_payload]
-            self.assertEqual(lis, val)
+            self.assertEqual(val, lis)
+
             prop_mock.assert_called_once()
             mock_get_json.assert_called_once()
 
@@ -60,11 +60,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_license(
-            self,
-            repo: Dict[str, str],
-            license_key: str,
-            expected: bool) -> None:
+    def test_has_license(self, repo, license_key, expected):
+        """ unit-test for GithubOrgClient.has_license """
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
 
@@ -88,7 +85,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         def side_effect_mock(url):
             return {
                     'https://api.github.com/orgs/google': cls.org_payload,
-                    'https://api.github.com/orgs/google/repos': cls.repos_payload,
+                    'https://api.github.com/orgs/google/\
+                            repos': cls.repos_payload,
                     }[url]
         cls.mock_get.side_effect = MagicMock(side_effect=side_effect_mock)
 
